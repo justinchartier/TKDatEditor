95999fde267a7ece684a792a5d2dfd19ba8001d8
justinchartier <46270868+justinchartier@users.noreply.github.com>
2018-12-30 18:39:52 -0500

    Add project files.

---
+++ DatEditor/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace DatEditor
+{
+    partial class DatEditor
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DatEditor));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listView = new System.Windows.Forms.ListView();
+            this.fileName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.fileSize = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_remove = new System.Windows.Forms.Button();
+            this.button_extract = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog2 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.toolStripSeparator2,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "&File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("newToolStripMenuItem.Image")));
+            this.newToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.newToolStripMenuItem.Text = "&New";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("openToolStripMenuItem.Image")));
+            this.openToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.O)));
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Image = ((System.Drawing.Image)(resources.GetObject("saveToolStripMenuItem.Image")));
+            this.saveToolStripMenuItem.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.S)));
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(143, 6);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "&Help";
+            this.helpToolStripMenuItem.Click += new System.EventHandler(this.helpToolStripMenuItem_Click);
+            // 
+            // listView
+            // 
+            this.listView.BackColor = System.Drawing.SystemColors.Window;
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.fileName,
+            this.fileSize});
+            this.listView.Location = new System.Drawing.Point(13, 28);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(259, 181);
+            this.listView.TabIndex = 1;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // fileName
+            // 
+            this.fileName.Text = "File Name";
+            this.fileName.Width = 155;
+            // 
+            // fileSize
+            // 
+            this.fileSize.Text = "File Size";
+            this.fileSize.Width = 100;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.Filter = "TK Dat File (* .dat) | * .dat";
+            this.openFileDialog1.Title = "Select a TK Dat file";
+            // 
+            // button_add
+            // 
+            this.button_add.Location = new System.Drawing.Point(107, 227);
+            this.button_add.Margin = new System.Windows.Forms.Padding(0);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(55, 25);
+            this.button_add.TabIndex = 2;
+            this.button_add.Text = "Add";
+            this.button_add.UseVisualStyleBackColor = true;
+            this.button_add.Click += new System.EventHandler(this.button_add_Click);
+            // 
+            // button_remove
+            // 
+            this.button_remove.Location = new System.Drawing.Point(162, 227);
+            this.button_remove.Margin = new System.Windows.Forms.Padding(0);
+            this.button_remove.Name = "button_remove";
+            this.button_remove.Size = new System.Drawing.Size(55, 25);
+            this.button_remove.TabIndex = 3;
+            this.button_remove.Text = "Remove";
+            this.button_remove.UseVisualStyleBackColor = true;
+            this.button_remove.Click += new System.EventHandler(this.button_remove_Click);
+            // 
+            // button_extract
+            // 
+            this.button_extract.Location = new System.Drawing.Point(217, 227);
+            this.button_extract.Margin = new System.Windows.Forms.Padding(0);
+            this.button_extract.Name = "button_extract";
+            this.button_extract.Size = new System.Drawing.Size(55, 25);
+            this.button_extract.TabIndex = 4;
+            this.button_extract.Text = "Extract";
+            this.button_extract.UseVisualStyleBackColor = true;
+            this.button_extract.Click += new System.EventHandler(this.button_extract_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "All Files (*.*)|*.*";
+            this.saveFileDialog1.Title = "Choose a save location";
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.Filter = "All Files (*.*)|*.*";
+            this.openFileDialog2.Title = "Choose a file to add";
+            // 
+            // saveFileDialog2
+            // 
+            this.saveFileDialog2.Filter = "TK Dat file (*.dat)|*.dat";
+            this.saveFileDialog2.Title = "Save Dat file location";
+            // 
+            // DatEditor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button_extract);
+            this.Controls.Add(this.button_remove);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.listView);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "DatEditor";
+            this.Text = "TK Dat Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader fileName;
+        private System.Windows.Forms.ColumnHeader fileSize;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_remove;
+        private System.Windows.Forms.Button button_extract;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog2;
+    }
+}
+
---
+++ DatEditor/Form1.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DatEditor
+{
+    public partial class DatEditor : Form
+    {
+        public DatEditor()
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open);
+                BinaryReader br = new BinaryReader(fs);
+                OpenDat(br);
+                br.Close();
+            }
+        }
+        private void button_add_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog2.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                addFileName = openFileDialog2.SafeFileName;
+                Add(openFileDialog2.FileName);
+            }
+        }
+        private void button_remove_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                ListViewItem item = listView.SelectedItems[0];
+                Remove(item);
+            }
+        }
+        private void button_extract_Click(object sender, EventArgs e)
+        {
+            if (listView.SelectedItems.Count > 0)
+            {
+                ListViewItem item = listView.SelectedItems[0];
+                saveFileDialog1.FileName = FileEntries[item.Index].Filename;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK && saveFileDialog1.FileName != "")
+                {
+                    Extract(FileEntries[item.Index], saveFileDialog1.FileName);
+                }
+            }
+        }
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog2.ShowDialog() == DialogResult.OK && saveFileDialog2.FileName != "")
+            {
+                Save(saveFileDialog2.FileName);
+            }
+        }
+
+        public struct FileEntry
+        {
+            public uint StartAddress;
+            public uint EndAddress;
+            public uint Filesize;
+            public string Filename;
+            public byte[] data;
+        }
+
+        public FileEntry[] FileEntries;
+        public string addFileName;
+
+        public void Save(string filename)
+        {
+            BinaryWriter bw = new BinaryWriter(File.Open(filename, FileMode.Create));
+            bw.Write(FileEntries.Count()+1);
+            
+            //write the header
+            uint startAddress = (uint)(17 * (FileEntries.Count()+1)) + 4;
+            uint writeAddress = startAddress;
+            string name;
+            byte[] buffer = new byte[13];
+            for (int i = 0; i < FileEntries.Count(); i++)
+            {
+                name = FileEntries[i].Filename;
+                buffer = ASCIIEncoding.ASCII.GetBytes(name);
+                if (buffer.Count() < 13)
+                {
+                    int diff = 13 - buffer.Count();
+                    Array.Resize<byte>(ref buffer, buffer.Count() + diff);
+                }
+                bw.Write(writeAddress);
+                bw.Write(buffer);
+                writeAddress = writeAddress + FileEntries[i].Filesize;
+            }
+
+            //write the null entry
+            bw.Write(writeAddress);
+            for (int i = 0; i < 13; i++)
+            {
+                bw.Write('\0');
+            }
+
+            //write the data
+            for (int i = 0; i < FileEntries.Count(); i++)
+            {
+                bw.Write(FileEntries[i].data);
+            }
+
+            bw.Close();
+        }
+        public void ClearDat()
+        {
+            listView.Items.Clear();
+            FileEntries = new FileEntry[0];
+        }
+
+        public void OpenDat(BinaryReader br)
+        {
+            ClearDat();
+            int totalCount;
+            int i;
+
+            totalCount = br.ReadInt32();
+            FileEntries = new FileEntry[(totalCount - 1)];
+
+            for (i = 0; i < (totalCount - 1); i++)
+            {
+                FileEntries[i].StartAddress = br.ReadUInt32();
+                string name = new string(br.ReadChars(13));
+                FileEntries[i].Filename = name;
+                FileEntries[i].EndAddress = br.ReadUInt32();
+                FileEntries[i].Filesize = FileEntries[i].EndAddress - FileEntries[i].StartAddress;
+                br.BaseStream.Seek(-4, SeekOrigin.Current);
+            }
+
+            for (i = 0; i < (totalCount - 1); i++)
+            {
+                br.BaseStream.Seek(FileEntries[i].StartAddress, SeekOrigin.Begin);
+                FileEntries[i].data = br.ReadBytes((int)FileEntries[i].Filesize);
+            }
+            RefreshList();
+        }
+
+        public void Add(string filepath)
+        {
+            FileStream fs = new FileStream(filepath, FileMode.Open);
+            BinaryReader br = new BinaryReader(fs);
+            FileEntry[] newfiles = new FileEntry[(FileEntries.Count()+1)];
+            int j = 0;
+            for (int i = 0; i < FileEntries.Count(); i++)
+            {
+                newfiles[j] = FileEntries[i];
+                j = j + 1;
+            }
+            newfiles[j].Filename = addFileName;
+            newfiles[j].Filesize = (uint)fs.Length;
+            newfiles[j].StartAddress = newfiles[(j - 1)].EndAddress;
+            newfiles[j].EndAddress = newfiles[j].StartAddress + newfiles[j].Filesize;
+            newfiles[j].data = br.ReadBytes((int)newfiles[j].Filesize);
+            FileEntries = newfiles;
+
+            ListViewItem item = new ListViewItem(FileEntries[j].Filename);
+            item.SubItems.Add(FileEntries[j].Filesize.ToString() + " bytes");
+            listView.Items.Add(item);
+
+            br.Close();
+        }
+        public void Extract(FileEntry file, string savepath)
+        {
+            BinaryWriter bw = new BinaryWriter(File.Open(savepath, FileMode.Create));
+            bw.Write(file.data);
+            bw.Close();
+        }
+
+        public void Remove(ListViewItem item)
+        {
+            FileEntry file = FileEntries[item.Index];
+            FileEntry[] newfiles = new FileEntry[(FileEntries.Count()-1)];
+            int j = 0;
+            for (int i = 0; i < FileEntries.Count(); i++)
+            {
+                if (!FileEntries[i].Equals(file))
+                {
+                    newfiles[j] = FileEntries[i];
+                    j = j + 1;
+                }
+            }
+            FileEntries = newfiles;
+            RefreshList();
+        }
+
+        public void RefreshList()
+        {
+            listView.Items.Clear();
+            if (FileEntries.Count() > 0)
+            {
+                for (int i = 0; i < FileEntries.Count(); i++)
+                {
+                    ListViewItem item = new ListViewItem(FileEntries[i].Filename);
+                    item.SubItems.Add(FileEntries[i].Filesize.ToString() + " bytes");
+                    listView.Items.Add(item);
+                }
+            }
+        }
+
+        private void helpToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string caption = "About TK Dat Editor";
+            string message = "TK Dat Editor v1.0\nWritten by Justin Chartier\n\nBased on Baram Dat Editor\nand information from Erik 'SiLo' Rogers\n\n2018";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result = MessageBox.Show(message, caption);
+        }
+    }
+}
